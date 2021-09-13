f2c739b02201cc732444c21939e4ae5667e1cf91
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-09-12 23:13:50 -0700

    Setting up my configurations in .env

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import movieRoutes from './routes/movies.js';
 
 
 // Initializing our express application
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -20,12 +22,12 @@ app.use('/movies', movieRoutes);
 // Connecting our server with a database
 // https://www.mongodb.com/cloud/atlas
 // atlas version of mongodb
-const CONNECTION_URL = 'mongodb+srv://muchumi:miz30594@cluster0.6cjvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 // Connecting to database
 // mongoose function accepts two parameters as indicated
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
